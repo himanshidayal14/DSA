c6759d33155551e31dd489da6552cbd85924d7b9
himanshidayal14 <himansheedayal@gmail.com>
2025-02-27 20:25:38 +0530

    added

---
+++ decimaltobinarymethods.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int decimaltobinarymethod1(int n)
+{
+    //division method
+    int binaryno=0;
+    int i=0;
+    while(n>0)
+    {
+        int bit=n%2;
+        binaryno=bit*pow(10,i++)+binaryno;
+        n=n/2;
+
+    }
+    return binaryno;
+}
+
+int decimaltobinarymethod2(int n){
+    //bitwise method
+    int binarynumber=0;
+    int i=0;
+    while (n>0)
+    {
+        int bit=(n&1);
+        binarynumber=bit*pow(10,i++)+binarynumber;
+        n=n>>1;
+    }
+    return binarynumber;
+}
+int main(){
+    int n;
+    cin>>n;
+    int binary=decimaltobinarymethod1(n);
+    int binary1=decimaltobinarymethod2(n);
+    cout<<binary<<endl<<binary1<<endl;
+
+}
